4f18da24f4bd7108c93d76aba414bc9bad7aa7d6
Daniel Klevebring <daniel.klevebring@gmail.com>
2016-06-28 13:38:39 +0200

    fix whitespace

--- setup.py
+++ setup.py
@@ -12,21 +12,22 @@ def parse_reqs(req_path='./requirements.txt'):
     """Recursively parse requirements from nested pip files."""
     install_requires = []
     with codecs.open(req_path, 'r') as handle:
-	# remove comments and empty lines
-	lines = (line.strip() for line in handle
-		 if line.strip() and not line.startswith('#'))
+        # remove comments and empty lines
+        lines = (line.strip() for line in handle
+                 if line.strip() and not line.startswith('#'))
 
-	for line in lines:
-	    # check for nested requirements files
-	    if line.startswith('-r'):
-		# recursively call this function
-		install_requires += parse_reqs(req_path=line[3:])
+        for line in lines:
+            # check for nested requirements files
+            if line.startswith('-r'):
+                # recursively call this function
+                install_requires += parse_reqs(req_path=line[3:])
 
-	    else:
-		# add the line as a new requirement
-		install_requires.append(line)
+            else:
+                # add the line as a new requirement
+                install_requires.append(line)
 
     return install_requires
+
 
 setup(name='genomicassertions',
       author='Daniel Klevebring',
